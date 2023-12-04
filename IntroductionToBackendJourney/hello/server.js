0f04e0ffade073ede865ea7eda2fc832df8ef3fd
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-12-04 11:41:39 +0600

    First Commit

---
+++ IntroductionToBackendJourney/hello/server.js
@@ -0,0 +1,22 @@
+const { log } = require("console");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<h1>Hello World</h1>");
+    res.statusCode = 200;
+    res.end();
+  } else if (req.url === "/hello") {
+    res.write("<h1>Hello Guest</h1>");
+    res.statusCode = 200;
+    res.end();
+  } else {
+    res.write("<h1>404 Not Found</h1>");
+    res.statusCode = 200;
+    res.end();
+  }
+});
+
+server.listen(8000, () => {
+  console.log("Listening to server 8000");
+});
