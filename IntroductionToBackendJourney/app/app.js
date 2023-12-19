571aca66d89193871d3a6569e9bff0136e92c435
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-12-19 17:33:35 +0600

    error handler added

--- IntroductionToBackendJourney/app/app.js
+++ IntroductionToBackendJourney/app/app.js
@@ -1,12 +1,44 @@
+require("dotenv").config("../.env");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use([morgan("dev"), cors(), express.json()]);
+
 app.get("/health", (_req, res) => {
   //if any case you don't use any obj use a (_) before the obj name
+  throw new Error("error");
   res.status(200).json({
     message: "Success",
   });
+});
+
+app.get("/health2", (_req, res) => {
+  //if any case you don't use any obj use a (_) before the obj name
+  throw new Error("error");
+  res.status(200).json({
+    message: "Success",
+  });
+});
+
+app.use((_req, _res, next) => {
+  //error handler middleware
+  const error = new Error("Resource not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, _req, res, _next) => {
+  //error handler middleware
+  if (error.status) {
+    return res.status(error.status).json({
+      message: error.message,
+    });
+  }
+
+  res.status(500).json({ message: "Something went wrong" });
 });
 
 module.exports = app;
--- IntroductionToBackendJourney/server.js
+++ IntroductionToBackendJourney/server.js
@@ -1,8 +1,12 @@
-const http =require('http')
-const app = require('./app/app')
+require("dotenv").config();
+const http = require("http");
+const app = require("./app/app");
+const { log } = require("console");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-server.listen(8000, ()=>{
-  
-})
+const PORT = process.env.PORT || 8888;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
